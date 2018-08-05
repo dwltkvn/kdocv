209a5d6ef2d2d930107a292212450ee5d43b0332
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-08-05 18:57:08 +0200

    add grommet dependencies + react template class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Split from "grommet/components/Split";
+import Box from "grommet/components/Box";
 
 import "./styles.css";
+import "../node_modules/grommet-css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Split
+      separator={false}
+      flex="right"
+      fixed={true}
+      priority="right"
+      showOnResponsive="priority"
+    >
+      <Box colorIndex="neutral-1" justify="center" align="center" pad="medium">
+        Left Side
+      </Box>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </Split>
   );
 }
 
