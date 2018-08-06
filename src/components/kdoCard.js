6c4996ae10411737f14a95b35f983a008e3fea83
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-08-06 18:38:18 +0200

    Work on KdoCard on mobile view

--- src/components/kdoCard.js
+++ src/components/kdoCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Box from "grommet/components/Anchor";
 import Card from "grommet/components/Card";
 import Section from "grommet/components/Box";
 import Anchor from "grommet/components/Anchor";
 
 import Pulse from "grommet/components/icons/Pulse";
 import IconPdf from "grommet/components/icons/base/Download";
+import IconDown from "grommet/components/icons/base/Down";
 
 import KdoSocial from "./kdoSocial";
 
@@ -18,9 +20,15 @@ class KdoCard extends React.Component {
   }
   //icon={<Pulse size="xsmall" icon={<IconPdf size="xsmall" />} />}
   render() {
-    const marg = this.props.small ? null : "small";
+    const marg = this.props.small ? "none" : "small";
     return (
-      <Section direction="column" pad={{ between: "medium" }} margin={marg}>
+      <Section
+        direction="column"
+        pad={{ between: "medium" }}
+        margin={marg}
+        full={this.props.small}
+        justify="between"
+      >
         <Card
           thumbnail="https://avatars2.githubusercontent.com/u/7428297?s=460&v=4"
           label="Dowlut Kevin"
@@ -35,6 +43,11 @@ class KdoCard extends React.Component {
           colorIndex="light-2"
         />
         <KdoSocial small={this.props.small} />
+        {this.props.small ? (
+          <Box alignSelf="center" align="center">
+            <IconDown />
+          </Box>
+        ) : null}
       </Section>
     );
   }
