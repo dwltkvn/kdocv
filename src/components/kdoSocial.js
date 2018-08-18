79549a62d4369df35f196495f3a9b103b57a5d5a
dwltkvn <kdo@gm.com>
2018-08-18 18:32:23 +0200

    work on the 'social' button - add a contact button

--- src/components/kdoSocial.js
+++ src/components/kdoSocial.js
@@ -5,10 +5,11 @@ import Box from "grommet/components/Box";
 import Button from "grommet/components/Button";
 
 import IconGithub from "grommet/components/icons/base/SocialGithub";
+import IconMail from "grommet/components/icons/base/Mail";
 
 // small stateless component that represent a social button. Props are icon, label and href
 const SocialAnchor = props => {
-  return <Button icon={props.icon} label={props.label} href={props.href} />;
+  return <Box margin={ {bottom:'small'} } alignSelf="center"><Button icon={props.icon} label={props.label} href={props.href}/></Box>;
 };
 
 class KdoSocial extends React.Component {
@@ -20,16 +21,15 @@ class KdoSocial extends React.Component {
 
   //
   render() {
-    const dir = this.props.small ? "row" : "column";
+    const dir = this.props.small ? "row" : "row";
     return (
       <Box
-        pad="small"
-        align="center"
+        pad="none"
         direction={dir}
-        justify="between"
-        margin="small"
-        responsive={false}
-        wrap={true}
+        justify="around"
+        margin="none"
+        responsive={true}
+        wrap={false}
       >
         <SocialAnchor
           small={this.props.small}
@@ -37,6 +37,14 @@ class KdoSocial extends React.Component {
           icon={<IconGithub />}
           href="https://github.com/dwltkvn"
         />
+        
+        <SocialAnchor
+          small={this.props.small}
+          label="/ contact"
+          icon={<IconMail />}
+          href="#contact"
+        />
+        
       </Box>
     );
   }
