90223b090b3edf308f24ce95305421338627142b
dwltkvn <kdo@gm.com>
2018-08-22 00:39:40 +0200

    remove the box component from the link anchor of the Card component because it caused the final build to fail

--- src/components/kdoCard.js
+++ src/components/kdoCard.js
@@ -39,11 +39,11 @@ class KdoCard extends React.Component {
           label="Dowlut Kevin"
           heading="Software Developer"
           description={<KdoSkillsTyping />}
-          link={<Box><IconPdf/>
+          link={
             <Anchor
               href="https://www.dropbox.com/s/bo2x8vyhyceqaly/CV_DOWLUT_Kevin.pdf?dl=0"
               label="Download CV"
-            /></Box>
+            />
           }
           colorIndex="light-2"
         />
